8815ab05a7f9cbbc8202d9212fd86e82d7ec62f6
Srubee Cakes <srubeecakes@gmail.com>
2026-06-14 19:45:22 +0530

    fix navbar hamburger

--- CAKES/src/components/Navbar.jsx
+++ CAKES/src/components/Navbar.jsx
@@ -1,38 +1,27 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
 
-      <h2 className="logo">
-        SRUBEE CAKES
-      </h2>
+      <h2 className="logo">SRUBEE CAKES</h2>
 
-      <div className="menu">
+      {/* hamburger button */}
+      <div className="toggle" onClick={() => setOpen(!open)}>
+        ☰
+      </div>
 
-        <NavLink to="/">
-          Home
-        </NavLink>
+      <div className={open ? "menu active" : "menu"}>
 
-        <NavLink to="/about">
-          About
-        </NavLink>
-
-        <NavLink to="/gallery">
-          Gallery
-        </NavLink>
-
-        <NavLink to="/order">
-          Order
-        </NavLink>
-
-        <NavLink to="/reviews">
-          Reviews
-        </NavLink>
-
-        <NavLink to="/contact">
-          Contact
-        </NavLink>
+        <NavLink to="/" onClick={() => setOpen(false)}>Home</NavLink>
+        <NavLink to="/about" onClick={() => setOpen(false)}>About</NavLink>
+        <NavLink to="/gallery" onClick={() => setOpen(false)}>Gallery</NavLink>
+        <NavLink to="/order" onClick={() => setOpen(false)}>Order</NavLink>
+        <NavLink to="/reviews" onClick={() => setOpen(false)}>Reviews</NavLink>
+        <NavLink to="/contact" onClick={() => setOpen(false)}>Contact</NavLink>
 
       </div>
 
